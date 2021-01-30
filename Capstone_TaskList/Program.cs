b7e487b6ce72766fa7e67b92f645cd09423daf43
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-01-30 12:54:27 -0500

    Updated with Extended Exercises

--- Capstone_TaskList/Program.cs
+++ Capstone_TaskList/Program.cs
@@ -20,7 +20,7 @@ namespace Capstone_TaskList
             menu.Add("Mark Task Complete");
             menu.Add("Quit");
             List<string> teamMembers = new List<string>();
-            teamMembers.Add("Team Member not listed. Add Another Team Member");
+            teamMembers.Add("Add Team Member");
             teamMembers.Add("Michael Scott");
             teamMembers.Add("Dwight Schrute");
             teamMembers.Add("Pam Beesley");
@@ -37,6 +37,10 @@ namespace Capstone_TaskList
             //assignments.Add(new Assignment("Pam Beesley", "Clean Keds", new DateTime(2015, 5, 28), false));
             //assignments.Add(new Assignment("Angela Martin", "Call to order the party planning commitee", new DateTime(2001, 09, 16), false));
 
+            List<string> assignmentFields = new List<string>();
+            assignmentFields.Add("Team member assigned to task:");
+            assignmentFields.Add("Task Description:");
+            assignmentFields.Add("Task Due Date:");
 
 
             Console.WriteLine("Welcome to the Task Manager \n");
@@ -59,7 +63,7 @@ namespace Capstone_TaskList
                 else if (menu[menuSelection - 1] == "Delete Task")
                 {
                     ListTasks(assignments);
-                    Console.WriteLine("Which Task would you like to delete?");
+                    Console.WriteLine("\nWhich of the above tasks would you like to delete?");
 
                     int userchoice = AssignmentNumberListChoiceValidation(assignments);
                     Console.WriteLine($"Are you sure you want to remove task {userchoice}: {assignments[userchoice-1].AssignmentDescription}?");
@@ -67,11 +71,11 @@ namespace Capstone_TaskList
                     if (yesNo == true)
                     {
                         assignments.RemoveAt(userchoice - 1);
-                        Console.WriteLine("Task Removed");
+                        Console.WriteLine("\nTask Removed\n");
                     } 
                     else if (yesNo == false)
                     { 
-                        Console.WriteLine("Returning to Main Menu");
+                        Console.WriteLine("Return to Main Menu");
                     }
                 }
                 else if (menu[menuSelection - 1] == "Mark Task Complete")
@@ -88,7 +92,7 @@ namespace Capstone_TaskList
                     }
                     else if (yesNo == false)
                     {
-                        Console.WriteLine("Returning to Main Menu");
+                        Console.WriteLine("Return to Main Menu");
                     }
                     
                 }
@@ -101,7 +105,143 @@ namespace Capstone_TaskList
                 {
                     AddTeamMember(teamMembers);
                 }
+                else if (menu[menuSelection - 1] == "Edit Task")
+                {
+                    ListTasks(assignments);
 
+                    
+                        Console.WriteLine("Which Task would you like to edit?");
+
+
+                        int taskToEdit = AssignmentNumberListChoiceValidation(assignments);
+                    while (true)
+                    {
+                        Console.WriteLine("Which field would you like to edit?");
+                        WriteList(assignmentFields);
+                        int fieldToEdit = NumberListChoiceValidation(assignmentFields);
+
+                        if (fieldToEdit == 3)
+                        {
+                            Console.WriteLine($"Please enter a new {assignmentFields[fieldToEdit - 1]}");
+                            DateTime taskDueDate = ValidateDate();
+                            assignments[taskToEdit - 1].DueDate = taskDueDate;
+                            Console.WriteLine($"New {assignmentFields[fieldToEdit-1]} {assignments[taskToEdit - 1].DueDate}\n");
+
+                        }
+                        else if (fieldToEdit == 1)
+                        {
+
+                            while (true)
+                            {
+                                Console.WriteLine("Who should the task be assigned to?");
+
+                                WriteList(teamMembers);
+                                int teamMemberNumber = NumberListChoiceValidation(teamMembers);
+
+                                if (teamMembers[teamMemberNumber - 1] == "Add Team Member")
+                                {
+                                    AddTeamMember(teamMembers);
+
+                                    continue;
+                                }
+                                string assignedTeamMember = teamMembers[teamMemberNumber - 1];
+                                assignments[taskToEdit - 1].TeamMember = assignedTeamMember;
+                                Console.WriteLine($"New {assignmentFields[fieldToEdit-1]} {assignments[taskToEdit - 1].TeamMember}\n");
+                                break;
+                            }
+                        }
+                        else if (fieldToEdit == 2)
+                        {
+                            Console.WriteLine($"Please enter a new {assignmentFields[fieldToEdit - 1]}");
+                            assignments[taskToEdit - 1].AssignmentDescription = Console.ReadLine();
+                            Console.WriteLine($"New {assignmentFields[fieldToEdit-1]} {assignments[taskToEdit - 1].AssignmentDescription}\n");
+                        }
+                        Console.WriteLine("Would you like to edit another field? (y/n)");
+                        bool moreEdits = YesNoValidation();
+                        if (moreEdits == true)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                   
+                }
+                else if (menu[menuSelection - 1] == "List Tasks due before a certain date")
+                {
+                    Console.WriteLine("List tasks due before: (enter date XX/XX/XXXX)");
+                    DateTime dueBeforeDate = ValidateDate();
+                    if (assignments.Count == 0)
+                    {
+                        Console.WriteLine("There are no working tasks, add task on the main menu");
+                    }
+                    int i = 1;
+                    foreach (Assignment task in assignments)
+                    {
+                        if (task.Complete == true && dueBeforeDate > task.DueDate)
+                        {
+                            string CompletionStatus = "Complete";
+                            Console.WriteLine($"Task {i}: {task.AssignmentDescription} \nDue Date: {task.DueDate.ToLongDateString()} \nAssigned to: {task.TeamMember} \nStatus: {CompletionStatus}\n");
+                            i++;
+                        }
+                        else if (task.Complete == false && dueBeforeDate > task.DueDate)
+                        {
+                            string CompletionStatus = "In Progess";
+                            Console.WriteLine($"Task {i}: {task.AssignmentDescription} \nDue Date: {task.DueDate.ToLongDateString()} \nAssigned to: {task.TeamMember} \nStatus: {CompletionStatus}\n");
+                            i++;
+                        }
+
+                    }
+                }
+                else if (menu[menuSelection - 1] == "List Task by Team Member")
+                {
+                    WriteList(teamMembers);
+
+                    Console.WriteLine("You would like to see all tasks assigned to which of the above team members?");
+                    int selectedTeamMember = NumberListChoiceValidation(teamMembers);
+                    if (teamMembers[selectedTeamMember - 1] == "Add Team Member")
+                    {
+                        AddTeamMember(teamMembers);
+
+                        continue;
+                    }
+                    if (assignments.Count == 0)
+                    {
+                        Console.WriteLine("There are no working tasks, add task on the main menu");
+                    }
+                    int i = 1;
+                    foreach (Assignment task in assignments)
+                    {
+                        if (task.Complete == true && task.TeamMember == teamMembers[selectedTeamMember-1])
+                        {
+                            string CompletionStatus = "Complete";
+                            Console.WriteLine($"Task {i}: {task.AssignmentDescription} \nDue Date: {task.DueDate.ToLongDateString()} \nAssigned to: {task.TeamMember} \nStatus: {CompletionStatus}\n");
+                            i++;
+                        }
+                        else if (task.Complete == false && task.TeamMember == teamMembers[selectedTeamMember-1])
+                        {
+                            string CompletionStatus = "In Progess";
+                            Console.WriteLine($"Task {i}: {task.AssignmentDescription} \nDue Date: {task.DueDate.ToLongDateString()} \nAssigned to: {task.TeamMember} \nStatus: {CompletionStatus}\n");
+                            i++;
+                        }
+
+                    }
+
+                }
+
+                Console.WriteLine("Would you like to continue? Press Y to return to the main menu. Press N to Quit.\n");
+                bool returnToMainMenu = YesNoValidation();
+                if (returnToMainMenu == true)
+                {
+                    Console.WriteLine();
+                    continue;
+                } 
+                else if (returnToMainMenu == false)
+                {
+                    break;
+                }
 
             }
 
@@ -176,7 +316,7 @@ namespace Capstone_TaskList
                 else
                 {
                     string CompletionStatus = "In Progess";
-                    Console.WriteLine($"Task {i}: {task.AssignmentDescription} \nDue Date: {task.DueDate.ToLongDateString()} \nAssigned to: {task.TeamMember} \n Status: {CompletionStatus}\n");
+                    Console.WriteLine($"Task {i}: {task.AssignmentDescription} \nDue Date: {task.DueDate.ToLongDateString()} \nAssigned to: {task.TeamMember} \nStatus: {CompletionStatus}\n");
                     i++;
                 }
                 
@@ -314,7 +454,7 @@ namespace Capstone_TaskList
                 }
                 else
                 {
-                    Console.WriteLine("Invalid Input! Are you Sure? Y/N");
+                    Console.WriteLine("Invalid Input! Y/N");
                     continue;
                 }
 
